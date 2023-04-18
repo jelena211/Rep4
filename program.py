f489b0b099757398d86c03ae0b2dd915c0af8863
Jelena Stamenkovic <jelena@Jelenas-iMac.local>
2023-04-18 23:31:33 +0200

    u program.py dodata fja rollDice()

--- program.py
+++ program.py
@@ -1,2 +1,11 @@
-for i in range(0,5):
-    print("bruji, bruji carsija: ", i)
+from random import randint
+
+def rollDice():
+    broj = 0
+    broj = randint(0,6)
+    return broj
+
+
+for i in range(0,15):
+    print("Broj je: ", rollDice())
+
